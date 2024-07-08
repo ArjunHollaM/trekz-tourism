5e832612432ee490d54bf7d6500ac7c5f0bd596b
ARJUN HOLLA <arjun.holla@mercedes-benz.com>
2024-07-08 15:16:23 +0530

    RUM Initialization

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: 'e987ffd4-3463-4579-9bb4-231395cc8ba0',
+    clientToken: 'pub56e8d69ab66fa9c70e7a3805efa20da0',
+    // `site` refers to the Datadog site parameter of your organization
+    // see https://docs.datadoghq.com/getting_started/site/
+    site: 'us5.datadoghq.com',
+    service: 'aicte-website',
+    env: 'dev',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    version: '0.1.0', 
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 50,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
